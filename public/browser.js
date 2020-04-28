1827807e8a3b76a3c6267248e81c7af2dcaecd3d
Romanas Golovko <ro@Romanass-MacBook-Pro.local>
2020-04-28 23:24:56 +0300

    Finished Update method

---
+++ public/browser.js
@@ -0,0 +1,12 @@
+document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('edit-me')) {
+        let userInput = prompt('Enter your desired new text', e.target.parentElement.parentElement.querySelector('.item-text').innerHTML);
+        if (userInput) {
+            axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id')}).then(function() {
+                e.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput;
+            }).catch(function() {
+                console.log('Please try again later');
+            });
+        }
+    }
+});
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ let mongodb = require('mongodb');
 let app = express();
 let db;
 
+app.use(express.static('public'));
+
 let connectionString = 'mongodb+srv://todoAppUser:ifHWWbAy3oj6FfGy@cluster0-c8sne.mongodb.net/TodoApp?retryWrites=true&w=majority';
 mongodb.connect(connectionString, {useNewUrlParser: true}, function(err, client) {
     db = client.db();
     app.listen(3000);
 });
 
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
@@ -42,7 +45,7 @@ app.get('/', function(req, res) {
                  <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                     <span class="item-text">${item.text}</span>
                     <div>
-                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
                     <button class="delete-me btn btn-danger btn-sm">Delete</button>
                     </div>
                  </li>
@@ -51,7 +54,8 @@ app.get('/', function(req, res) {
             </ul>
             
           </div>
-          
+          <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+          <script src="/browser.js"></script>
         </body>
         </html>
         `);    
@@ -65,3 +69,8 @@ app.post('/create-item', function(req, res) {
     });
 });
 
+app.post('/update-item', function(req, res) {
+    db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+        res.send('Success');
+    });
+});
